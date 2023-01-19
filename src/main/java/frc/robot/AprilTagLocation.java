c48f0d77ddcff4469eb430557fba17d49a6041b2
Team1091 <team1091@gmail.com>
2023-01-18 19:11:53 -0600

    added AprilTagLocation

--- src/main/java/frc/robot/AprilTagLocation.java
+++ src/main/java/frc/robot/AprilTagLocation.java
@@ -1,5 +1,19 @@
 package frc.robot;
 
+import java.util.List;
+
 public class AprilTagLocation {
-    public int number=0;
+    private int idNumber;
+    private double targetDistance;
+    public AprilTagLocation(int idNumber, double targetDistance) {
+       idNumber = this.idNumber;
+       targetDistance = this.targetDistance;
+    }
+    public int getIdNumber(){
+        return idNumber;
+    }
+
+    public double getTargetDistance(){
+        return targetDistance;
+    }
 }
