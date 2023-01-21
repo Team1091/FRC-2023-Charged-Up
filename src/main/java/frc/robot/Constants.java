4c688b235ed0f17fb4d7c387915c8b3055130d98
patelhar <harshyboi2004@gmail.com>
2023-01-21 12:02:54 -0600

    Added Gyro stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,8 +4,7 @@
 
 package frc.robot;
 
-import frc.robot.utils.Distance;
-import frc.robot.utils.Rotation;
+import edu.wpi.first.wpilibj.SPI;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -15,29 +14,32 @@ import frc.robot.utils.Rotation;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public class Constants { //put final here
+public final class Constants {
     public static double deadzone = 0.1;
 
     public static final class DriveTrain {
-        public final static int backLeftMotorChannel = 2;
-        public final static int frontLeftMotorChannel = 1;
-        public final static int frontRightMotorChannel = 4;
-        public final static int backRightMotorChannel = 3;
-        public final static double originTolerance = 0.3;
-    }
-
-    public static class OperatorConstants {
-        public static final int kDriverControllerPort = 0;
-    }
+    public final static int backLeftMotorChannel = 1;
+    public final static int frontLeftMotorChannel = 2;
+    public final static int frontRightMotorChannel = 3;
+    public final static int backRightMotorChannel = 4;
+    public final static double originTolerance = 0.3;
+  }
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
 
 
     public final static String cameraName = "Microsoft_LifeCam_HD-3000";
     public final static int cameraPixelHeight = 180;
     public final static int cameraPixelWidth = 640;
     //public final static double cameraHeightMeters = 0.2;
-    public static Distance cameraHeight = Distance.inMeters(.249);
-    public static Rotation cameraPitch = Rotation.inDegrees(0.0);
-    public final static Distance targetHeight = Distance.inMeters(.384175);
-    //public static double targetHeightInMeters = 0.8128;
-    public final static Distance stationTargetHeight = Distance.inMeters(.619252);
+    public static double cameraHeightMeters = 0.74422;
+    public static double cameraPitchRadians = 0.0;
+    //public final static double targetHeightInMeters = 0.384175;
+    public static double targetHeightInMeters = 0.8128;
+    public final static double stationTargetHeightInMeters = 0.619252;
+
+    public final static SPI.Port gyroPort = SPI.Port.kMXP;
+    public final static double gyroDeadZoneThreshhold = 10;
+    public final static double gyroCommandMoveSpeed = 0.25;
 }
--- src/main/java/frc/robot/subsystems/GyroSubsystem.java
+++ src/main/java/frc/robot/subsystems/GyroSubsystem.java
@@ -4,18 +4,21 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 
-public class GyroBalanceSubsystem extends SubsystemBase {
+public class GyroSubsystem extends SubsystemBase {
     /**
      * Creates a new ExampleSubsystem.
      */
 
-    //AHRS gyro = new AHRS(SPI.Port.kMXP);
-    public GyroBalanceSubsystem() {
-
+    private final AHRS gyro;
+    public GyroSubsystem() {
+        gyro = new AHRS(Constants.gyroPort);
     }
 
     /**
@@ -50,5 +53,13 @@ public class GyroBalanceSubsystem extends SubsystemBase {
     @Override
     public void simulationPeriodic() {
         // This method will be called once per scheduler run during simulation
+    }
+
+    public double getRoll() {
+        return gyro.getRoll();
+    }
+
+    public double getPitch() {
+        return gyro.getPitch();
     }
 }
