6bf407b60250d5caf8e45d963102c518ca1d10d7
BradyNo <ratzmbra@huhs.org>
2023-02-18 08:18:04 -0600

    Added an auto command for sussex that makes us just move out of the community

---
+++ src/main/java/frc/robot/commands/AutoStartCommands/SussexDummyAutoCommand.java
@@ -0,0 +1,14 @@
+package frc.robot.commands.AutoStartCommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.DistanceDriveCommand;
+import frc.robot.subsystems.DriveTrainSubsystem;
+
+public class SussexDummyAutoCommand extends SequentialCommandGroup {
+    public SussexDummyAutoCommand(DriveTrainSubsystem driveTrainSubsystem){
+        addCommands(
+                new DistanceDriveCommand(driveTrainSubsystem,50.0)
+        );
+
+    }
+}
